97bd312ea0c5213fff2271d11969172628e10106
mkdedona <128740829+mkdedona@users.noreply.github.com>
2023-03-29 18:48:18 +0000

    29/3/23 apuntes

---
+++ operadores.py
@@ -0,0 +1,21 @@
+# OPERADORES MATEMATICOS
+
+# + suma 
+# - resta
+# * multiplicacion
+# / division
+# % modulo, devueleve el resto de la division
+# ** potenciacion
+# // division baja divide redondeando para abajo siempre
+#
+
+# = se usa para asignar valores
+
+# OPERDADORES DE COMPARACION devuelven verdadero y falso
+#
+# == es igual a
+# != es diferente a
+# < es menor a
+# <= es menor o igual a
+# > es mayor a
+# >= es mayor o igual
